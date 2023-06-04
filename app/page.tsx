3b4ce2756144a52862b83f5e61a6daa2f375b221
dewodt <dewodt@users.noreply.github.com>
2023-06-04 09:09:16 +0000

    Prettified Code!

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main className="flex-auto py-8 px-5">
-      TES
-    </main>
-  );
+  return <main className="flex-auto px-5 py-8">TES</main>;
 }
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -27,7 +27,7 @@ const NavBar = () => {
     },
   ];
   return (
-    <nav className="sticky top-0 z-30 flex h-20 w-full flex-none flex-row items-center justify-between shadow-lg bg-custom-blue px-7 lg:px-10">
+    <nav className="sticky top-0 z-30 flex h-20 w-full flex-none flex-row items-center justify-between bg-custom-blue px-7 shadow-lg lg:px-10">
       {/* TOASTI Logo */}
       <Link href="/">
         <Image
