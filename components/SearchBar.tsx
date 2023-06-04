158e35428b7c55ac6b66ffe985b350b7e6a2e9fb
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2023-06-04 22:51:32 +0700

    Created searchbar component.

---
+++ components/SearchBar.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+const SearchBar = () => {
+  const tagsExample = [
+    "alam semesta",
+    "astaga",
+    "aphelion",
+    "astrobiologi",
+    "astrofisika",
+    "astronomi posisi",
+  ];
+  const [searchValue, setSearchValue] = useState(""); // String value in Search Bar
+  const [tagsResult, setTagsResult] = useState<Array<string>>([]); // Tags Recommendation
+
+  return (
+    <div className="relative flex flex-row items-center gap-3 rounded-xl bg-custom-white p-3">
+      {/* Search Icon */}
+      <div className="relative h-[20px] w-[20px] flex-none">
+        <Image src="/search.svg" alt="Search Icon" fill={true} />
+      </div>
+
+      {/* Search Input */}
+      <input
+        className="flex-auto bg-custom-white font-poppins-bold text-lg text-custom-blue focus:outline-none"
+        type="text"
+        placeholder="Cari Artikel"
+        value={searchValue}
+        onChange={(e) => {
+          const newValue = e.target.value;
+          const newTagResults = tagsExample.filter((item) => {
+            return item.toLowerCase().startsWith(newValue.toLowerCase());
+          });
+          setSearchValue(newValue);
+          setTagsResult(newTagResults);
+        }}
+      />
+
+      {/* Reset Icon */}
+      <button
+        className="relative h-[20px] w-[20px] flex-none"
+        aria-label="Reset Button"
+        onClick={() => {
+          setSearchValue("");
+          setTagsResult([]);
+        }}
+      >
+        <Image src="/reset.svg" alt="Reset Icon" fill={true} />
+      </button>
+
+      {/* Tag Recommendation */}
+      {/* Only show when search is not empty and tagsResult is not empty */}
+      {searchValue && tagsResult.length !== 0 && (
+        <div className="absolute left-8 top-16 h-fit w-fit rounded-2xl bg-custom-white px-4 py-2">
+          {tagsResult.map((item, index) => {
+            return (
+              <div
+                key={index}
+                className="py-1 font-poppins-bold text-lg text-custom-blue"
+                onClick={() => {
+                  setSearchValue(item);
+                  setTagsResult([]);
+                }}
+              >
+                {item}
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SearchBar;
