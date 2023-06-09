67db7912386f448ac43f1f70da584b7ac9de4756
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2023-06-09 22:55:35 +0700

    Fixed search bug: Doesn't update filteredArticles when clicked on search recommendation.

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -16,14 +16,17 @@ const SearchBar = ({
   setSearchValue: Dispatch<SetStateAction<string>>;
   setFilteredArticles: Dispatch<SetStateAction<Array<Article>>>;
 }) => {
-  // Tags Recommendation
+  // Tags Recommendation (LOWER CASE)
   const [tagsResult, setTagsResult] = useState<Array<string>>([]);
 
-  // Collect all Tags
+  // Collect allTags from AllArticles and convert to lowercase
   const allTagsCombined: Array<string> = [];
   allArticles.forEach((article) => allTagsCombined.push(...article.tags));
   const allTagsUnique = allTagsCombined.filter((value, index, self) => {
     return self.indexOf(value) === index;
+  });
+  const allTagsUniqueLowerCase = allTagsUnique.map((tag) => {
+    return tag.toLowerCase();
   });
 
   return (
@@ -46,8 +49,8 @@ const SearchBar = ({
           const newSearchValueLowerCase = newSearchValue.toLowerCase();
 
           // Update Tags Recommendation (case insensitive, must match from start)
-          const newTagResults = allTagsUnique.filter((tag) => {
-            return tag.toLowerCase().startsWith(newSearchValueLowerCase);
+          const newTagResults = allTagsUniqueLowerCase.filter((tag) => {
+            return tag.startsWith(newSearchValueLowerCase);
           });
           setTagsResult(newTagResults);
 
@@ -97,17 +100,47 @@ const SearchBar = ({
       {/* Only show when search is not empty and tagsResult is not empty */}
       {searchValue && tagsResult.length !== 0 && (
         <div className="absolute left-8 top-16 h-fit w-fit rounded-2xl bg-custom-white px-4 py-2">
-          {tagsResult.map((item, index) => {
+          {tagsResult.map((tag, index) => {
             return (
               <div
                 key={index}
                 className="py-1 font-poppins-bold text-lg text-custom-blue"
                 onClick={() => {
-                  setSearchValue(item);
+                  // Update Search State
+                  setSearchValue(tag);
+
+                  // Update Recommendation Tag
                   setTagsResult([]);
+
+                  // Update Filtered Article Result
+                  // Accepts title, date, tags, author
+                  const newFilteredArticles = allArticles.filter((article) => {
+                    const isTitleIncludes = article.title
+                      .toLowerCase()
+                      .includes(tag);
+                    const isDateIncludes = new Date(article._firstPublishedAt)
+                      .toLocaleString("id-ID", { dateStyle: "long" })
+                      .toLowerCase()
+                      .includes(tag);
+                    const isAuthorIncludes = article.author
+                      .toLowerCase()
+                      .includes(tag);
+                    const isTagsStartsMatch =
+                      article.tags.filter((articleTag) =>
+                        articleTag.toLowerCase().startsWith(tag)
+                      ).length !== 0;
+
+                    return (
+                      isTitleIncludes ||
+                      isDateIncludes ||
+                      isAuthorIncludes ||
+                      isTagsStartsMatch
+                    );
+                  });
+                  setFilteredArticles(newFilteredArticles);
                 }}
               >
-                {item}
+                {tag}
               </div>
             );
           })}
