00244ac7b6693005d82d47ee6e88373b12a0ee26
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2023-06-22 08:56:55 +0700

    Add title to nearest astronomy phenomena section.

--- app/kalender-astronomi/page.tsx
+++ app/kalender-astronomi/page.tsx
@@ -19,45 +19,50 @@ const KalenderAstronomi = async () => {
         </section>
 
         {/* 4 Nearest future events */}
-        <ul
+        <section
           data-aos="zoom-in"
-          className="flex w-full max-w-sm flex-col gap-8 sm:max-w-md lg:max-w-sm lg:gap-10 xl:max-w-md 2xl:max-w-lg"
+          className="flex flex-col items-center gap-8 lg:gap-10 lg:self-start"
         >
-          {allAstronomyCalendars
-            .filter((event) => {
-              const timeEvent = new Date(event.date).getTime();
-              const timeNow = new Date().getTime();
-              return timeEvent >= timeNow;
-            })
-            .slice(0, 4)
-            .map((event, index) => {
-              return (
-                <li
-                  key={index}
-                  className="flex flex-row items-center gap-6 xl:gap-9"
-                >
-                  <Image
-                    className="h-16 w-16 rounded-full object-cover xl:h-24 xl:w-24"
-                    alt={event.image.alt}
-                    src={event.image.url}
-                    width={event.image.width}
-                    height={event.image.height}
-                  />
-                  <div className="flex w-full flex-col gap-1 text-custom-white xl:gap-2">
-                    <div className="line-clamp-1 break-all font-poppins-bold text-2xl xl:text-3xl">
-                      {event.title}
+          <h3 className="border-b-4 border-solid border-custom-pink pb-2 text-center font-poppins-bold text-3xl text-custom-white lg:self-start xl:pb-3 xl:text-5xl xl:leading-tight">
+            Fenomena Terdekat
+          </h3>
+          <ul className="flex w-full max-w-sm flex-col gap-8 sm:max-w-md lg:max-w-sm lg:gap-10 xl:max-w-md 2xl:max-w-lg">
+            {allAstronomyCalendars
+              .filter((event) => {
+                const timeEvent = new Date(event.date).getTime();
+                const timeNow = new Date().getTime();
+                return timeEvent >= timeNow;
+              })
+              .slice(0, 4)
+              .map((event, index) => {
+                return (
+                  <li
+                    key={index}
+                    className="flex flex-row items-center gap-6 xl:gap-9"
+                  >
+                    <Image
+                      className="h-16 w-16 rounded-full object-cover xl:h-24 xl:w-24"
+                      alt={event.image.alt}
+                      src={event.image.url}
+                      width={event.image.width}
+                      height={event.image.height}
+                    />
+                    <div className="flex w-full flex-col gap-1 text-custom-white xl:gap-2">
+                      <div className="line-clamp-1 break-all font-poppins-bold text-xl xl:text-2xl">
+                        {event.title}
+                      </div>
+                      <div className="line-clamp-1 break-all font-inter-medium text-base xl:text-lg">
+                        {new Date(event.date).toLocaleString("id-ID", {
+                          dateStyle: "long",
+                        })}
+                      </div>
                     </div>
-                    <div className="line-clamp-1 break-all font-inter-medium text-base xl:text-lg">
-                      {new Date(event.date).toLocaleString("id-ID", {
-                        dateStyle: "long",
-                      })}
-                    </div>
-                  </div>
-                  <CalendarButton color="pink" event={event} />
-                </li>
-              );
-            })}
-        </ul>
+                    <CalendarButton color="pink" event={event} />
+                  </li>
+                );
+              })}
+          </ul>
+        </section>
       </div>
     </main>
   );
