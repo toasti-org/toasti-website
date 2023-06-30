e6a545a25cf4d60f628318e7ff3d36439a5f4b35
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2023-07-01 03:44:32 +0700

    Set Session expiration to 1 hour (Same as google access token age).

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,6 +3,26 @@ import type { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 const authOptions: AuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: "" + process.env.GOOGLE_CLIENT_ID,
+      clientSecret: "" + process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "select_account",
+          scope:
+            "https://www.googleapis.com/auth/calendar.events.owned https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile",
+        },
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+    maxAge: 3600,
+  },
+  jwt: {
+    maxAge: 3600,
+  },
   callbacks: {
     jwt: async ({ token, account }) => {
       // Initialize access token and id when sign in
@@ -15,18 +35,6 @@ const authOptions: AuthOptions = {
       return session;
     },
   },
-  providers: [
-    GoogleProvider({
-      clientId: "" + process.env.GOOGLE_CLIENT_ID,
-      clientSecret: "" + process.env.GOOGLE_CLIENT_SECRET,
-      authorization: {
-        params: {
-          scope:
-            "https://www.googleapis.com/auth/calendar.events.owned https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile",
-        },
-      },
-    }),
-  ],
 };
 
 const handler = NextAuth(authOptions);
